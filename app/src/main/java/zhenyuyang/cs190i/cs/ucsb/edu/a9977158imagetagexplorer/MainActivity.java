1667e6ab00c2f9bb55490ae8e4ec61547b15b2f7
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-05-15 12:16:01 -0700

    use  Picasso in gridView

--- app/src/main/java/zhenyuyang/cs190i/cs/ucsb/edu/a9977158imagetagexplorer/MainActivity.java
+++ app/src/main/java/zhenyuyang/cs190i/cs/ucsb/edu/a9977158imagetagexplorer/MainActivity.java
@@ -2,6 +2,7 @@ package zhenyuyang.cs190i.cs.ucsb.edu.a9977158imagetagexplorer;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
@@ -15,7 +16,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,11 +33,22 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     static Random rnd = new Random();
-
+//    String[] web = {
+//            "Google"
+//    } ;
+    Uri[] imageUris;
+    GridView grid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageTagDatabaseHelper.Initialize(this);
+        final ImageTagDatabaseHelper dbHelper = ImageTagDatabaseHelper.GetInstance();
+
+//        //  write information
+//        SQLiteDatabase database_w;
+//        database_w = dbHelper.getWritableDatabase();
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -42,104 +56,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
 
-                ImageTagDatabaseHelper dbHelper = ImageTagDatabaseHelper.GetInstance();
+//                ImageTagDatabaseHelper dbHelper = ImageTagDatabaseHelper.GetInstance();
+//
+//                //  write information
+//                SQLiteDatabase database_w;
+//                database_w = dbHelper.getWritableDatabase();
 
-                //  write information
-                SQLiteDatabase database_w;
-                database_w = dbHelper.getWritableDatabase();
-
-                    String[] imageUris = getImageUriFromDBAtIndex(1,dbHelper.getReadableDatabase());
+                imageUris = getImageUriFromDB(dbHelper.getReadableDatabase());
 
                 getAllOnlineResource();
+
+//
+
             }
         });
+
+        imageUris = getImageUriFromDB(dbHelper.getReadableDatabase());
+        // ==============    GridView   =====================
+        SelelctImageGrid adapter = new SelelctImageGrid(this, imageUris);
+        grid=(GridView)findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Log.i("addOnItemTouchListener", "onItemClick position =" + position);
+
+
+            }
+        });
+
+// ==============   End of GridView   =====================
 
 
         // ==============    ImageTagDatabaseHelper   =====================
 
-        ImageTagDatabaseHelper.Initialize(this);
-        //ImageTagDatabaseHelper h = new ImageTagDatabaseHelper(this);
-        ImageTagDatabaseHelper dbHelper = ImageTagDatabaseHelper.GetInstance();
-
-
-//        SQLiteDatabase db1 = dbHelper.getReadableDatabase();
-//        String[] selectionArgs1 = { "1" };
-//        Cursor cursor1 = db1.query(
-//                "asdf",                     // The table to query
-//                selectionArgs1,                               // The columns to return
-//                "asdf",                                // The columns for the WHERE clause
-//                selectionArgs1,                            // The values for the WHERE clause
-//                null,                                     // don't group the rows
-//                null,                                     // don't filter by row groups
-//                "asdf"                                 // The sort order
-//        );
-
-        //  write information
-        SQLiteDatabase database_w;
-        database_w = dbHelper.getWritableDatabase();
-        Log.i("SQLiteDatabase", "database = " + database_w.toString());
-
-
-        // Insert the new row, returning the primary key value of the new row
-        String tableName_insert = "Image";
-
-        // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        String column_name_insert = "ImageUri";
-        values.put(column_name_insert, "http://cs.jalexander.ninja:8080/");
-//        column_name_insert = "COLUMN_NAME_SUBTITLE";
-//        values.put(column_name_insert, "testSubtitle");
-
-        try {
-            long newRowId = database_w.insertOrThrow(tableName_insert, null, values);
-            Log.i("SQLiteDatabase", "newRowId = " + newRowId);
-        } catch (Exception e) {
-            Log.i("SQLiteDatabase", "Exception = " + e.toString());
-            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
-        }
-
-        // end of write information
-
-        //  read information
-
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                "Id",
-                "ImageUri",
-        };
-
-        // Filter results WHERE "title" = 'My Title'
-        String column_name_read_filter = "Id";
-        String selection = column_name_read_filter + " = ?";
-        String[] selectionArgs = {"2"};
-
-// How you want the results sorted in the resulting Cursor
-        String sortOrder =
-                "Id" + " DESC";
-        String tableName_read = "Image";
-        Cursor cursor = db.query(
-                tableName_read,                     // The table to query
-                projection,                               // The columns to return
-                selection,                                // The columns for the WHERE clause
-                selectionArgs,                            // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                sortOrder                                 // The sort order
-        );
-        Log.i("cursor", "cursor !");
-        List itemIds = new ArrayList<>();
-        while (cursor.moveToNext()) {
-            long itemId = cursor.getLong(cursor.getColumnIndexOrThrow("Id"));
-            String ss = cursor.getString(cursor.getColumnIndexOrThrow("ImageUri"));
-            Log.i("cursor", "cursor ss = " + ss);
-            itemIds.add(itemId);
-        }
-        cursor.close();
-
-        // end of read information
 
 
 // *****************  ImageTagDatabaseHelper  Test *****************
@@ -211,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         final TextView textView = (TextView) findViewById(R.id.textView);
-        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+       // final ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
 
         TaggedImageRetriever.getNumImages(new TaggedImageRetriever.ImageNumResultListener() {
@@ -236,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
 
                     String filePathURI = Uri.fromFile(getFileStreamPath("Test.jpg")).toString();
                     Log.i("onTaggedImage", "getFileStreamPath(\"Test.jpg\") = " + filePathURI);
-                    Picasso.with(MainActivity.this).load(filePathURI).resize(500, 500).centerCrop().into(imageView);
+                   // Picasso.with(MainActivity.this).load(filePathURI).resize(500, 500).centerCrop().into(imageView);
                     // imageView.setImageBitmap(image.image);
                     StringBuilder tagList = new StringBuilder();
                     for (String p : image.tags) {
@@ -259,6 +211,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         ImageTagDatabaseHelper.GetInstance().close();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ImageTagDatabaseHelper.GetInstance().close();
+
     }
 
     @Override
@@ -327,12 +286,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     void saveImageUriToDB(String uri_input, SQLiteDatabase database_w) {
-//        ImageTagDatabaseHelper.Initialize(this);
-//        ImageTagDatabaseHelper dbHelper = ImageTagDatabaseHelper.GetInstance();
-//
-//        //  write information
-//        SQLiteDatabase database_w;
-//        database_w = dbHelper.getWritableDatabase();
 
         Log.i("SQLiteDatabase", "database = " + database_w.toString());
 
@@ -350,11 +303,9 @@ public class MainActivity extends AppCompatActivity {
             Log.i("SQLiteDatabase", "Exception = " + e.toString());
             Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
         }
-        // end of write information
-
     }
 
-    String[]  getImageUriFromDBAtIndex(int index, SQLiteDatabase db) {
+    Uri[]  getImageUriFromDBAtIndex(int index, SQLiteDatabase db) {
 //  read information
         String[] projection = {
                 "Id",
@@ -385,17 +336,17 @@ public class MainActivity extends AppCompatActivity {
         Cursor  cursor = db.rawQuery(query,null);
 
         Log.i("cursor", "cursor !");
-        ArrayList<String> itemIds = new ArrayList<String>();
+        ArrayList<Uri> itemIds = new ArrayList<Uri>();
         while (cursor.moveToNext()) {
            // long itemId = cursor.getLong(cursor.getColumnIndexOrThrow("Id"));
             //String ss = cursor.getString(cursor.getColumnIndexOrThrow("ImageUri"));
-            String ss =  cursor.getString(cursor.getColumnIndex("ImageUri"));
+            Uri ss =  Uri.parse(cursor.getString(cursor.getColumnIndex("ImageUri")));
             Log.i("cursor", "1cursor ImageUri = " + ss);
             itemIds.add(ss);
         }
         cursor.close();
         // end of read information
-        String[] uris = itemIds.toArray(new String[itemIds.size()]);
+        Uri[] uris = itemIds.toArray(new Uri[itemIds.size()]);
 
 //        for(int i = 0; i< uris.length;i++){
 //            Log.i("cursor", "uris["+i+"] = " + uris[i]);
@@ -404,5 +355,55 @@ public class MainActivity extends AppCompatActivity {
         return uris;
     }
 
+
+    Uri[]  getImageUriFromDB( SQLiteDatabase db) {
+//  read information
+        String[] projection = {
+                "Id",
+                "ImageUri",
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String column_name_read_filter = "Id";
+        String selection = column_name_read_filter + " = *";
+
+
+// How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                "Id" + " DESC";
+        String tableName_read = "Image";
+//        Cursor cursor = db.query(
+//                tableName_read,                     // The table to query
+//                projection,                               // The columns to return
+//                selection,                                // The columns for the WHERE clause
+//                selectionArgs,                            // The values for the WHERE clause
+//                null,                                     // don't group the rows
+//                null,                                     // don't filter by row groups
+//                sortOrder                                 // The sort order
+//        );
+
+        String query = "SELECT * FROM "+tableName_read;
+
+        Cursor  cursor = db.rawQuery(query,null);
+
+        Log.i("cursor", "cursor !");
+        ArrayList<Uri> itemIds = new ArrayList<Uri>();
+        while (cursor.moveToNext()) {
+            // long itemId = cursor.getLong(cursor.getColumnIndexOrThrow("Id"));
+            //String ss = cursor.getString(cursor.getColumnIndexOrThrow("ImageUri"));
+            Uri ss =  Uri.parse(cursor.getString(cursor.getColumnIndex("ImageUri")));
+            Log.i("cursor", "1cursor ImageUri = " + ss);
+            itemIds.add(ss);
+        }
+        cursor.close();
+        // end of read information
+        Uri[] uris = itemIds.toArray(new Uri[itemIds.size()]);
+
+//        for(int i = 0; i< uris.length;i++){
+//            Log.i("cursor", "uris["+i+"] = " + uris[i]);
+//        }
+
+        return uris;
+    }
 
 }
---
+++ app/src/main/java/zhenyuyang/cs190i/cs/ucsb/edu/a9977158imagetagexplorer/SelelctImageGrid.java
@@ -0,0 +1,98 @@
+package zhenyuyang.cs190i.cs.ucsb.edu.a9977158imagetagexplorer;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Zhenyu on 2017-05-14.
+ */
+
+public class SelelctImageGrid extends BaseAdapter {
+    private Context mContext;
+    private final String[] web;
+    private  int[] Imageid;
+    private  Uri[] ImageUris;
+
+
+
+
+
+    public SelelctImageGrid(Context c,String[] web,int[] Imageid ) {
+        mContext = c;
+        this.Imageid = Imageid;
+        this.web = web;
+    }
+
+    public SelelctImageGrid(Context c,String[] web,Uri[] ImageUris ) {
+        mContext = c;
+        this.ImageUris = ImageUris;
+        this.web = web;
+    }
+
+    public SelelctImageGrid(Context c,Uri[] ImageUris ) {
+        mContext = c;
+        this.ImageUris = ImageUris;
+        this.web = null;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return ImageUris.length+1;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            if(position!=ImageUris.length) {
+                grid = new View(mContext);
+                grid = inflater.inflate(R.layout.grid_single, null);
+                //TextView textView = (TextView) grid.findViewById(R.id.grid_text);
+                ImageView imageView = (ImageView) grid.findViewById(R.id.grid_image);
+                //textView.setText(web[position]);
+                //imageView.setImageResource(Imageid[position]);
+                //imageView.setImageURI(ImageUris[position]);
+                Picasso.with(parent.getContext()).load(ImageUris[position]).fit().into(imageView);
+            }
+            else{
+                grid = new View(mContext);
+                grid = inflater.inflate(R.layout.grid_single, null);
+                //TextView textView = (TextView) grid.findViewById(R.id.grid_text);
+                ImageView imageView = (ImageView) grid.findViewById(R.id.grid_image);
+                //textView.setText(web[position]);
+                imageView.setBackgroundColor(Color.GRAY);
+                imageView.setImageResource(R.drawable.kong);
+                //imageView.setImageURI(ImageUris[position]);
+            }
+        } else {
+            grid = (View) convertView;
+        }
+
+        return grid;
+    }
+}
